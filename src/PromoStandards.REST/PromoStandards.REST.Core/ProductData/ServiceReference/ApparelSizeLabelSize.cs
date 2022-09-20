131b713179aa7cd3bd2b3b94f4e233af8b9cd0b9
Mehmet Büber <mehmetbuber@gmail.com>
2022-09-20 04:10:40 +0300

    ProductData2.0.0 Models

---
+++ src/PromoStandards.REST/PromoStandards.REST.Core/ProductData/ServiceReference/ApparelSizeLabelSize.cs
@@ -0,0 +1,67 @@
+﻿/// <remarks/>
+[System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Tools.ServiceModel.Svcutil", "2.0.3")]
+[System.Xml.Serialization.XmlTypeAttribute(Namespace="http://www.promostandards.org/WSDL/ProductDataService/2.0.0/SharedObjects/")]
+public enum ApparelSizeLabelSize
+{
+    
+    /// <remarks/>
+    OSFA,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("6XS")]
+    Item6XS,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("5XS")]
+    Item5XS,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("4XS")]
+    Item4XS,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("3XS")]
+    Item3XS,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("2XS")]
+    Item2XS,
+    
+    /// <remarks/>
+    XS,
+    
+    /// <remarks/>
+    S,
+    
+    /// <remarks/>
+    M,
+    
+    /// <remarks/>
+    L,
+    
+    /// <remarks/>
+    XL,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("2XL")]
+    Item2XL,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("3XL")]
+    Item3XL,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("4XL")]
+    Item4XL,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("5XL")]
+    Item5XL,
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlEnumAttribute("6XL")]
+    Item6XL,
+    
+    /// <remarks/>
+    CUSTOM,
+}
---
+++ src/PromoStandards.REST/PromoStandards.REST.Core/ProductData/ServiceReference/GetProductDateModifiedResponse.cs
@@ -0,0 +1,41 @@
+﻿/// <remarks/>
+[System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Tools.ServiceModel.Svcutil", "2.0.3")]
+[System.Diagnostics.DebuggerStepThroughAttribute()]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType=true, Namespace="http://www.promostandards.org/WSDL/ProductDataService/2.0.0/")]
+public partial class GetProductDateModifiedResponse
+{
+    
+    private ProductDateModified[] productDateModifiedArrayField;
+    
+    private ServiceMessage[] serviceMessageArrayField;
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlArrayAttribute(Order=0)]
+    [System.Xml.Serialization.XmlArrayItemAttribute("ProductDateModified", IsNullable=false)]
+    public ProductDateModified[] ProductDateModifiedArray
+    {
+        get
+        {
+            return this.productDateModifiedArrayField;
+        }
+        set
+        {
+            this.productDateModifiedArrayField = value;
+        }
+    }
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlArrayAttribute(Namespace="http://www.promostandards.org/WSDL/ProductDataService/2.0.0/SharedObjects/", Order=1)]
+    [System.Xml.Serialization.XmlArrayItemAttribute("ServiceMessage", IsNullable=false)]
+    public ServiceMessage[] ServiceMessageArray
+    {
+        get
+        {
+            return this.serviceMessageArrayField;
+        }
+        set
+        {
+            this.serviceMessageArrayField = value;
+        }
+    }
+}
