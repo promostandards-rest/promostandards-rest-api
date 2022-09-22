1cd6675e2634825bca10d8b183e7c258ae49b04e
Steven Zarichney <szarichney@hard-line.com>
2022-09-22 00:14:08 -0400

    start auto expand after a second

--- src/PromoStandards.REST/PromoStandards.REST.API/wwwroot/custom.js
+++ src/PromoStandards.REST/PromoStandards.REST.API/wwwroot/custom.js
@@ -1,7 +1,9 @@
-﻿ (() => {
-    const i = setInterval(() => {
-      [...document.querySelectorAll('.models [aria-expanded="false"]')].map((el) =>
-        el.click()
-      ).length || clearInterval(i);
+﻿(() => {
+    setTimeout(() => {
+        const i = setInterval(() => {
+            [...document.querySelectorAll('.models [aria-expanded="false"]')].map((el) =>
+                el.click()
+            ).length || clearInterval(i);
+        }, 1000);
     }, 1000);
 })();
