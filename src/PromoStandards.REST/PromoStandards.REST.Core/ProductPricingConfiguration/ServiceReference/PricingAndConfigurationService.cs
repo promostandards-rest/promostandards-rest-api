c0446044633b4528c8e1bdbac7696adbe448a579
Mehmet Büber <mehmetbuber@gmail.com>
2022-09-23 03:53:52 +0300

    Removed unused interfaces

--- src/PromoStandards.REST/PromoStandards.REST.Core/ProductPricingConfiguration/ServiceReference/PricingAndConfigurationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PromoStandards.REST.Core.ProductPricingConfiguration.ServiceReference;
-public interface PricingAndConfigurationService
-{
-    System.Threading.Tasks.Task<GetAvailableLocationsResponse> getAvailableLocationsAsync(GetAvailableLocationsRequest request);
-    System.Threading.Tasks.Task<GetDecorationColorsResponse> getDecorationColorsAsync(GetDecorationColorsRequest request);
-    System.Threading.Tasks.Task<GetFobPointsResponse> getFobPointsAsync(GetFobPointsRequest request);
-    System.Threading.Tasks.Task<GetAvailableChargesResponse> getAvailableChargesAsync(GetAvailableChargesRequest request);
-    System.Threading.Tasks.Task<GetConfigurationAndPricingResponse> getConfigurationAndPricingAsync(GetConfigurationAndPricingRequest request);
-}
