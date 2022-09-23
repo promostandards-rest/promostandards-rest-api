782ef5db0183b5ef24be755106e441376b12f100
Mehmet Büber <mehmetbuber@gmail.com>
2022-09-23 04:15:48 +0300

    Reverted mongodb app back

--- src/PromoStandards.REST/PromoStandards.REST.MongoDBApp/MongoDBConverter.cs
+++ src/PromoStandards.REST/PromoStandards.REST.MongoDBApp/MongoDBConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.ServiceModel;
+using System.Text.Json;
 using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 using PromoStandards.REST.MongoDB.ProductData;
@@ -11,6 +12,12 @@ namespace PromoStandards.REST.MongoDBApp
     {
         private readonly MongoDBProductService _productService;
         private readonly MongoDBConverterConfig _config;
+        private static BasicHttpsBinding BasicHttpsBinding = new BasicHttpsBinding()
+        {
+            MaxReceivedMessageSize = int.MaxValue,
+            MaxBufferPoolSize = int.MaxValue,
+            MaxBufferSize = int.MaxValue,
+        };
 
         public MongoDBConverter(IOptions<MongoDBConverterConfig> config, MongoDBProductService productService)
         {
@@ -20,79 +27,80 @@ namespace PromoStandards.REST.MongoDBApp
 
         public async Task Process()
         {
-            var basicHttpsBinding = new BasicHttpsBinding()
+            await ProductData();
+        }
+
+        private async Task ProductData()
+        {
+            var client = new ProductDataServiceClient(BasicHttpsBinding, new EndpointAddress(_config.Endpoint));
+
+            var sellable = await client.getProductSellableAsync(new GetProductSellableRequest()
             {
-                MaxReceivedMessageSize = int.MaxValue,
-                MaxBufferPoolSize = int.MaxValue,
-                MaxBufferSize = int.MaxValue,
-            };
+                wsVersion = _config.WsVersion,
+                id = _config.Id,
+                password = _config.Password,
+                isSellable = true,
+                localizationCountry = _config.LocalizationCountry,
+                localizationLanguage = _config.LocalizationLanguage
+            });
+            var sellableIds = sellable.GetProductSellableResponse.ProductSellableArray.Select(p => p.productId).ToList();
+            foreach (var productId in sellableIds)
+            {
+                try
+                {
+                    var product = await client.getProductAsync(new GetProductRequest
+                    {
+                        wsVersion = _config.WsVersion,
+                        id = _config.Id,
+                        password = _config.Password,
+                        localizationCountry = _config.LocalizationCountry,
+                        localizationLanguage = _config.LocalizationLanguage,
+                        productId = productId
+                    });
+                    var extendedProduct =
+                        JsonSerializer.Deserialize<ProductExtended>(
+                            JsonSerializer.Serialize(product.GetProductResponse.Product));
+                    extendedProduct.isSellable = true;
+                    await _productService.InsertUpdateProduct(extendedProduct);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
 
-            //var client = new ServiceReference.ProductDataServiceClient(basicHttpsBinding, new EndpointAddress(_config.Endpoint));
-
-            //var sellable = await client.getProductSellableAsync(new ServiceReference.GetProductSellableRequest()
-            //{
-            //    wsVersion = _config.WsVersion,
-            //    id = _config.Id,
-            //    password = _config.Password,
-            //    isSellable = true,
-            //    localizationCountry = _config.LocalizationCountry,
-            //    localizationLanguage = _config.LocalizationLanguage
-            //});
-            //var sellableIds = sellable.GetProductSellableResponse.ProductSellableArray.Select(p => p.productId).ToList();
-            //foreach (var productId in sellableIds)
-            //{
-            //    try
-            //    {
-            //        var product = await client.getProductAsync(new GetProductRequest
-            //        {
-            //            wsVersion = _config.WsVersion,
-            //            id = _config.Id,
-            //            password = _config.Password,
-            //            localizationCountry = _config.LocalizationCountry,
-            //            localizationLanguage = _config.LocalizationLanguage,
-            //            productId = productId
-            //        });
-            //        var extendedProduct = JsonSerializer.Deserialize<ProductExtended>(JsonSerializer.Serialize(product.GetProductResponse.Product));
-            //        extendedProduct.isSellable = true;
-            //        await _productService.InsertUpdateProduct(extendedProduct);
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Console.WriteLine(e);
-            //    }
-            //}
-            //var nonSellable = await client.getProductSellableAsync(new ServiceReference.GetProductSellableRequest()
-            //{
-            //    wsVersion = _config.WsVersion,
-            //    id = _config.Id,
-            //    password = _config.Password,
-            //    isSellable = false,
-            //    localizationCountry = _config.LocalizationCountry,
-            //    localizationLanguage = _config.LocalizationLanguage
-            //});
-            //var nonSellableIds = nonSellable.GetProductSellableResponse.ProductSellableArray.Select(p => p.productId).ToList();
-            //foreach (var productId in nonSellableIds)
-            //{
-            //    try
-            //    {
-            //        var product = await client.getProductAsync(new GetProductRequest
-            //        {
-            //            wsVersion = _config.WsVersion,
-            //            id = _config.Id,
-            //            password = _config.Password,
-            //            localizationCountry = _config.LocalizationCountry,
-            //            localizationLanguage = _config.LocalizationLanguage,
-            //            productId = productId
-            //        });
-            //        var extendedProduct = JsonSerializer.Deserialize<ProductExtended>(JsonSerializer.Serialize(product));
-            //        extendedProduct.isSellable = false;
-            //        await _productService.InsertUpdateProduct(extendedProduct);
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Console.WriteLine(e);
-            //    }
-            //}
+            var nonSellable = await client.getProductSellableAsync(new GetProductSellableRequest()
+            {
+                wsVersion = _config.WsVersion,
+                id = _config.Id,
+                password = _config.Password,
+                isSellable = false,
+                localizationCountry = _config.LocalizationCountry,
+                localizationLanguage = _config.LocalizationLanguage
+            });
+            var nonSellableIds = nonSellable.GetProductSellableResponse.ProductSellableArray.Select(p => p.productId).ToList();
+            foreach (var productId in nonSellableIds)
+            {
+                try
+                {
+                    var product = await client.getProductAsync(new GetProductRequest
+                    {
+                        wsVersion = _config.WsVersion,
+                        id = _config.Id,
+                        password = _config.Password,
+                        localizationCountry = _config.LocalizationCountry,
+                        localizationLanguage = _config.LocalizationLanguage,
+                        productId = productId
+                    });
+                    var extendedProduct = JsonSerializer.Deserialize<ProductExtended>(JsonSerializer.Serialize(product));
+                    extendedProduct.isSellable = false;
+                    await _productService.InsertUpdateProduct(extendedProduct);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
         }
     }
 }
---
+++ src/PromoStandards.REST/PromoStandards.REST.MongoDBApp/ProductData/ServiceReference/ProductDataServiceClient.cs
@@ -0,0 +1,148 @@
+﻿[System.Diagnostics.DebuggerStepThroughAttribute()]
+[System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Tools.ServiceModel.Svcutil", "2.0.3")]
+public partial class ProductDataServiceClient : System.ServiceModel.ClientBase<ProductDataService>, ProductDataService
+{
+    
+    /// <summary>
+    /// Implement this partial method to configure the service endpoint.
+    /// </summary>
+    /// <param name="serviceEndpoint">The endpoint to configure</param>
+    /// <param name="clientCredentials">The client credentials</param>
+    static partial void ConfigureEndpoint(System.ServiceModel.Description.ServiceEndpoint serviceEndpoint, System.ServiceModel.Description.ClientCredentials clientCredentials);
+    
+    public ProductDataServiceClient() : 
+        base(ProductDataServiceClient.GetDefaultBinding(), ProductDataServiceClient.GetDefaultEndpointAddress())
+    {
+        this.Endpoint.Name = EndpointConfiguration.ProductDataServiceBinding.ToString();
+        ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+    }
+    
+    public ProductDataServiceClient(EndpointConfiguration endpointConfiguration) : 
+        base(ProductDataServiceClient.GetBindingForEndpoint(endpointConfiguration), ProductDataServiceClient.GetEndpointAddress(endpointConfiguration))
+    {
+        this.Endpoint.Name = endpointConfiguration.ToString();
+        ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+    }
+    
+    public ProductDataServiceClient(EndpointConfiguration endpointConfiguration, string remoteAddress) : 
+        base(ProductDataServiceClient.GetBindingForEndpoint(endpointConfiguration), new System.ServiceModel.EndpointAddress(remoteAddress))
+    {
+        this.Endpoint.Name = endpointConfiguration.ToString();
+        ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+    }
+    
+    public ProductDataServiceClient(EndpointConfiguration endpointConfiguration, System.ServiceModel.EndpointAddress remoteAddress) : 
+        base(ProductDataServiceClient.GetBindingForEndpoint(endpointConfiguration), remoteAddress)
+    {
+        this.Endpoint.Name = endpointConfiguration.ToString();
+        ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+    }
+    
+    public ProductDataServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
+        base(binding, remoteAddress)
+    {
+    }
+    
+    [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+    System.Threading.Tasks.Task<getProductResponse1> ProductDataService.getProductAsync(getProductRequest1 request)
+    {
+        return base.Channel.getProductAsync(request);
+    }
+    
+    public System.Threading.Tasks.Task<getProductResponse1> getProductAsync(GetProductRequest GetProductRequest)
+    {
+        getProductRequest1 inValue = new getProductRequest1();
+        inValue.GetProductRequest = GetProductRequest;
+        return ((ProductDataService)(this)).getProductAsync(inValue);
+    }
+    
+    [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+    System.Threading.Tasks.Task<getProductDateModifiedResponse1> ProductDataService.getProductDateModifiedAsync(getProductDateModifiedRequest1 request)
+    {
+        return base.Channel.getProductDateModifiedAsync(request);
+    }
+    
+    public System.Threading.Tasks.Task<getProductDateModifiedResponse1> getProductDateModifiedAsync(GetProductDateModifiedRequest GetProductDateModifiedRequest)
+    {
+        getProductDateModifiedRequest1 inValue = new getProductDateModifiedRequest1();
+        inValue.GetProductDateModifiedRequest = GetProductDateModifiedRequest;
+        return ((ProductDataService)(this)).getProductDateModifiedAsync(inValue);
+    }
+    
+    [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+    System.Threading.Tasks.Task<getProductCloseOutResponse1> ProductDataService.getProductCloseOutAsync(getProductCloseOutRequest1 request)
+    {
+        return base.Channel.getProductCloseOutAsync(request);
+    }
+    
+    public System.Threading.Tasks.Task<getProductCloseOutResponse1> getProductCloseOutAsync(GetProductCloseOutRequest GetProductCloseOutRequest)
+    {
+        getProductCloseOutRequest1 inValue = new getProductCloseOutRequest1();
+        inValue.GetProductCloseOutRequest = GetProductCloseOutRequest;
+        return ((ProductDataService)(this)).getProductCloseOutAsync(inValue);
+    }
+    
+    [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+    System.Threading.Tasks.Task<getProductSellableResponse1> ProductDataService.getProductSellableAsync(getProductSellableRequest1 request)
+    {
+        return base.Channel.getProductSellableAsync(request);
+    }
+    
+    public System.Threading.Tasks.Task<getProductSellableResponse1> getProductSellableAsync(GetProductSellableRequest GetProductSellableRequest)
+    {
+        getProductSellableRequest1 inValue = new getProductSellableRequest1();
+        inValue.GetProductSellableRequest = GetProductSellableRequest;
+        return ((ProductDataService)(this)).getProductSellableAsync(inValue);
+    }
+    
+    public virtual System.Threading.Tasks.Task OpenAsync()
+    {
+        return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
+    }
+    
+    public virtual System.Threading.Tasks.Task CloseAsync()
+    {
+        return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
+    }
+    
+    private static System.ServiceModel.Channels.Binding GetBindingForEndpoint(EndpointConfiguration endpointConfiguration)
+    {
+        if ((endpointConfiguration == EndpointConfiguration.ProductDataServiceBinding))
+        {
+            System.ServiceModel.BasicHttpBinding result = new System.ServiceModel.BasicHttpBinding();
+            result.MaxBufferSize = int.MaxValue;
+            result.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+            result.MaxReceivedMessageSize = int.MaxValue;
+            result.AllowCookies = true;
+            result.Security.Mode = System.ServiceModel.BasicHttpSecurityMode.Transport;
+            return result;
+        }
+        throw new System.InvalidOperationException(string.Format("Could not find endpoint with name \'{0}\'.", endpointConfiguration));
+    }
+    
+    private static System.ServiceModel.EndpointAddress GetEndpointAddress(EndpointConfiguration endpointConfiguration)
+    {
+        if ((endpointConfiguration == EndpointConfiguration.ProductDataServiceBinding))
+        {
+            return new System.ServiceModel.EndpointAddress("https://services.starline.com/CustomerProductDataService/CustomerProductDataServi" +
+                                                           "ceV2.svc");
+        }
+        throw new System.InvalidOperationException(string.Format("Could not find endpoint with name \'{0}\'.", endpointConfiguration));
+    }
+    
+    private static System.ServiceModel.Channels.Binding GetDefaultBinding()
+    {
+        return ProductDataServiceClient.GetBindingForEndpoint(EndpointConfiguration.ProductDataServiceBinding);
+    }
+    
+    private static System.ServiceModel.EndpointAddress GetDefaultEndpointAddress()
+    {
+        return ProductDataServiceClient.GetEndpointAddress(EndpointConfiguration.ProductDataServiceBinding);
+    }
+    
+    public enum EndpointConfiguration
+    {
+        
+        ProductDataServiceBinding,
+    }
+}
