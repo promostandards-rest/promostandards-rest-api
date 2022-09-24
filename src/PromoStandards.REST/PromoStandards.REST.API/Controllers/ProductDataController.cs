ef8f7f06ef85015577a61a6fdd7955605c5badee
Mehmet Büber <mehmetbuber@gmail.com>
2022-09-24 04:00:46 +0300

    Video endpoint added

--- src/PromoStandards.REST/PromoStandards.REST.API/Controllers/ProductDataController.cs
+++ src/PromoStandards.REST/PromoStandards.REST.API/Controllers/ProductDataController.cs
@@ -304,6 +304,54 @@ namespace PromoStandards.REST.API.Controllers
             }
         }
 
+        /// <summary>
+        /// Provides the videos of a specific ProductId provided in the request.
+        /// </summary>
+        /// <param name="productId">The supplier's ID for a given product</param>
+        /// <param name="cultureName"></param>
+        /// <param name="partId"></param>
+        /// <param name="classType"></param>
+        /// <param name="lastModifiedDate"></param>
+        /// <remarks>
+        /// </remarks>
+        /// <response code="200">Returns product videos</response>
+        /// <response code="204">Product does not have any videos</response>
+        /// <response code="404">Product using productId is not found</response>
+        /// <response code="500">Internal Server Error</response>
+        [HttpGet("{productId}/videos")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CollectionResponse<MediaContent>>> GetVideos(String productId, String? cultureName, String? partId, int? classType, DateTime? lastModifiedDate)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(productId))
+                {
+                    return BadRequest("ProductId Required");
+                }
+                var response = await _mediaContentService.GetProductMedia(
+                    productId,
+                    mediaTypeType.Video,
+                    partId,
+                    cultureName,
+                    classType,
+                    lastModifiedDate
+                );
+                if (response == null)
+                {
+                    return new NoContentResult();
+                }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
 
         ///// <summary>
         ///// Provides the videos of a specific ProductId provided in the request.
