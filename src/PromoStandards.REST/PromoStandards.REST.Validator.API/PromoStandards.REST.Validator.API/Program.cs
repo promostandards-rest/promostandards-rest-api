a34007609aee946e4e4a77b16b906140c0da0c7c
YufengGuan <yufengguan@outlook.com>
2022-09-25 17:50:46 -0400

    added allow *

--- src/PromoStandards.REST/PromoStandards.REST.Validator.API/PromoStandards.REST.Validator.API/Program.cs
+++ src/PromoStandards.REST/PromoStandards.REST.Validator.API/PromoStandards.REST.Validator.API/Program.cs
@@ -1,19 +1,30 @@
 using PromoStandards.REST.Validator.API.Service;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<ISchemaService, SchemaService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader(); ;
+                      });
+});
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
